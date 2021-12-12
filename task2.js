17f14619d77b5bb69f6df65d82dc3c8dfe33a9f3
amin515 <nil.somudra51@gmail.com>
2021-12-12 22:05:56 +0600

    add json file

---
+++ task2.js
@@ -0,0 +1,328 @@
+
+
+
+ const student = [
+    {
+
+      "name"   :  "Nur Amin",
+      "roll"   :  20,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 40,
+      "en"     : 60,
+      "math"   : 45,
+      "s"      : 65,
+      "ss"     : 74,
+      "rel"    : 65
+
+      }]
+
+    },
+    {
+
+      "name"   :  "Humayun Kabir",
+      "roll"   :  34,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 44,
+      "en"     : 73,
+      "math"   : 45,
+      "s"      : 95,
+      "ss"     : 45,
+      "rel"    : 45
+
+      }]
+
+    },  {
+
+      "name"   :  "abu Sufian",
+      "roll"   :  24,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 45,
+      "en"     : 47,
+      "math"   : 51,
+      "s"      : 62,
+      "ss"     : 54,
+      "rel"    : 61
+
+      }]
+
+    },  {
+
+      "name"   :  "Sabbir Rahman",
+      "roll"   :  32,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 75,
+      "en"     : 63,
+      "math"   : 54,
+      "s"      : 69,
+      "ss"     : 77,
+      "rel"    : 36
+
+      }]
+
+    },  {
+
+      "name"   :  "Shahriar Somrat",
+      "roll"   :  19,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 38,
+      "en"     : 65,
+      "math"   : 51,
+      "s"      : 56,
+      "ss"     : 72,
+      "rel"    : 52
+
+      }]
+
+    },  {
+
+      "name"   :  "Abu Achem",
+      "roll"   :  31,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 51,
+      "en"     : 95,
+      "math"   : 54,
+      "s"      : 69,
+      "ss"     : 85,
+      "rel"    : 32
+
+      }]
+
+    },  {
+
+      "name"   :  "Mamun Islam",
+      "roll"   :  32,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 49,
+      "en"     : 66,
+      "math"   : 45,
+      "s"      : 45,
+      "ss"     : 71,
+      "rel"    : 62
+
+      }]
+
+    },  {
+
+      "name"   :  "Abu Raihan",
+      "roll"   :  23,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 41,
+      "en"     : 63,
+      "math"   : 51,
+      "s"      : 60,
+      "ss"     : 45,
+      "rel"    : 34
+
+      }]
+
+    },  {
+
+      "name"   :  "Sumaya tasneem",
+      "roll"   :  29,
+      "gender" :  "Female",
+      "marks"  : [{
+
+      "bn"     : 43,
+      "en"     : 64,
+      "math"   : 65,
+      "s"      : 54,
+      "ss"     : 47,
+      "rel"    : 40
+
+      }]
+
+    },  {
+
+      "name"   :  "Abu Sayeed",
+      "roll"   :  25,
+      "gender" :  "Male",
+      "marks"  : [{
+
+      "bn"     : 50,
+      "en"     : 66,
+      "math"   : 55,
+      "s"      : 95,
+      "ss"     : 74,
+      "rel"    : 35
+
+      }]
+
+    }
+    
+  ];
+
+  const family = [
+    {
+      name      : "Shahriar Somrat",
+      rel       : "Friends",
+      phone     : "0178546532",
+      location  : "Bogura"
+    },
+    
+    {
+      name      : "Abu Achem",
+      rel       : "Friends",
+      phone     : "0113252464",
+      location  : "Bogura"
+    },
+    {
+      name      : "Nur Amin",
+      rel       : "Friends",
+      phone     : "01303292586",
+      location  : "Bogura"
+    },  {
+      name      : "Sabbir Rahman",
+      rel       : "Friends",
+      phone     : "017875463",
+      location  : "Chittagong"
+    },  {
+      name      : "Sumaya Tasneem",
+      rel       : "Friends",
+      phone     : "0170011258",
+      location  : "Bogura"
+    },  {
+      name      : "Abu Raihan",
+      rel       : "Friends",
+      phone     : "0125488654",
+      location  : "Netrokona"
+    },  {
+      name      : "Humayun Kabir",
+      rel       : "Friends",
+      phone     : "011524876",
+      location  : "Sylhet"
+    },  {
+      name      : "Mamun Islam",
+      rel       : "Friends",
+      phone     : "0178543236",
+      location  : "Rangpur"
+    },  {
+      name      : "Abu Sufian",
+      rel       : "Friends",
+      phone     : "017526485",
+      location  : "Dhaka"
+    },  {
+      name      : "Abu Sayeed",
+      rel       : "Friends",
+      phone     : "+99-0214587",
+      location  : "Cumilla"
+    },
+
+  ];
+
+  const devs = [
+    {
+ 
+      id       : 1,
+      name     : 'Nur Amin',
+      age      : 32,
+      skill    : 'MERN Stack',
+      location : 'Bogura'
+
+    },
+    {
+ 
+      id       : 1,
+      name     : 'Shariar Somarat',
+      age      : 32,
+      skill    : 'IOS',
+      location : 'Bogura'
+
+    },
+    {
+ 
+      id       : 3,
+      name     : 'Sabbir Rahman',
+      age      : 32,
+      skill    : 'Linux',
+      location : 'Rangpur'
+
+    },
+    {
+ 
+      id       : 4,
+      name     : 'Sumaya Tasneem',
+      age      : 42,
+      skill    : 'Node.js',
+      location : 'Chuadanga'
+
+    },
+    {
+ 
+      id       : 5,
+      name     : 'Humayun Kabir',
+      age      : 32,
+      skill    : 'mongoDB',
+      location : 'Tangail'
+
+    },
+    {
+ 
+      id       : 6,
+      name     : 'Abu Sayeed',
+      age      : 29,
+      skill    : 'React.js',
+      location : 'Cumilla'
+
+    },
+    {
+ 
+      id       : 7,
+      name     : 'Abu Achem',
+      age      : 37,
+      skill    : 'JavaScript',
+      location : 'Dhaka'
+
+    },{
+ 
+      id       : 8,
+      name     : 'Abu Raihan',
+      age      : 34,
+      skill    : 'Django',
+      location : 'Sherpur'
+
+    },
+    {
+ 
+      id       : 9,
+      name     : 'Mamun Islam',
+      age      : 31,
+      skill    : 'Java',
+      location : 'Rankgpur'
+
+    },
+    {
+ 
+      id       : 10,
+      name     : 'Abu Sufian',
+      age      : 35,
+      skill    : 'Python',
+      location : 'Cumilla'
+
+    }
+    
+  ];
+
+
+
+ let data = JSON.stringify(family);
+ let data1 = JSON.stringify(devs);
+ let data2 = JSON.stringify(student)
+ 
+console.log(data1);
+
